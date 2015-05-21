3a14faf5d2d37601a462d9727af9b5639ae081a7
karthik venna <karthikvenna@koyadocutracker.com>
2015-05-21 15:00:26 +0530

    karthik first commit

---
+++ Testproject/Testproject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Testproject.Global" Language="C#" %>
---
+++ Testproject/Testproject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testproject.Startup))]
+namespace Testproject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
