ec1a485d3e3951fd6511469366b8dc4a55f84bae
vkarthik-sailfish <vkarthik-sailfish@users.noreply.github.com>
2015-05-21 16:57:05 +0530

    i updated these changes
    changing things

--- Testproject/Testproject/Models/IdentityModels.cs
+++ Testproject/Testproject/Models/IdentityModels.cs
@@ -18,12 +18,15 @@ namespace Testproject.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = manager.CreateIdentity(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            //return userIdentity;
+            
             return userIdentity;
         }
 
         public Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager manager)
         {
-            return Task.FromResult(GenerateUserIdentity(manager));
+          //  return Task.FromResult(GenerateUserIdentity(manager));
+          return Task.FromResult(GenerateUserIdentity(manager));
         }
     }
 
